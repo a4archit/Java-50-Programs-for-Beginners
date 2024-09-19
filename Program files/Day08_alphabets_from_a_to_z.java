15c58ed6fac45f235b37171af46a09fc70d40a13
Archit <115338571+a4archit@users.noreply.github.com>
2024-09-19 17:08:07 +0530

    Create Day08_alphabets_from_a_to_z.java

---
+++ Program files/Day08_alphabets_from_a_to_z.java
@@ -0,0 +1,11 @@
+class Day08_alphabets_from_a_to_z {
+    
+    public static void main(String[] args) {
+        
+        for (char ch='A'; ch<='Z'; ch++){
+            System.out.print(ch + " ");
+        }  
+        
+    }
+    
+}
