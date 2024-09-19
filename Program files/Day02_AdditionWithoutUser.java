35767e7c09ad7b575e2487c820bef42b2f1602fe
Archit <115338571+a4archit@users.noreply.github.com>
2024-09-19 11:13:56 +0530

    Create Day02_AdditionWithoutUser.java

---
+++ Program files/Day02_AdditionWithoutUser.java
@@ -0,0 +1,12 @@
+
+class Day02_AdditionWithoutUser {
+    public static void main(String[] args) {
+        // declaring and assigning variables
+        int a = 10;
+        int b = 20;
+        
+        int c = a+b;
+        
+        System.out.println(c);
+    }
+}
