d5dcb8f32d784df191f8d195c2e21ed7c1906851
Archit <115338571+a4archit@users.noreply.github.com>
2024-09-21 18:23:14 +0530

    Create Day18_area_of_circle.java

---
+++ Program files/Day18_area_of_circle.java
@@ -0,0 +1,15 @@
+class Day18_area_of_circle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        float r;
+        double area;
+        
+        System.out.print("Enter radius: ");
+        r = sc.nextFloat();
+        
+        area = Math.round(Math.PI * Math.pow(r,2));
+        
+        System.out.println("\nArea of circle = "+area);
+    }
+}
