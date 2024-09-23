3c85ac11b52b8056cead5b02d6ce5659f836ce50
Archit <115338571+a4archit@users.noreply.github.com>
2024-09-23 08:35:03 +0530

    Update Day22_swapping.java

--- Program files/Day22_swapping.java
+++ Program files/Day22_swapping.java
@@ -1,2 +1,24 @@
-// swapping of two numbers
-// with third variable
+import java.util.Scanner;
+
+class Day22_swapping {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+         
+        int a,b;
+
+        a = 10;
+        b = 20;
+        
+        // displaying numbers before swapping
+        System.out.println("\n\nBefore swapping \n\ta = " + a + "\n\tb = " + b);
+        
+        // swapping
+        int temp = a;
+        a = b;
+        b = temp;
+        
+        System.out.println("\n\nAfter swapping\n\ta = " + a + "\n\tb = " + b);
+        
+    }
+}    
+    
