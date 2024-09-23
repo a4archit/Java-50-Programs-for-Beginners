19d4f1b39c2523d56887ffb5b43dc854d40bab87
Archit <115338571+a4archit@users.noreply.github.com>
2024-09-23 08:16:46 +0530

    Update Day21_area_of_triangle.java

--- Program files/Day21_area_of_triangle.java
+++ Program files/Day21_area_of_triangle.java
@@ -1,2 +1,20 @@
-// area of triangle
+
+import java.util.Scanner;
+
+class Day21_area_of_triangle {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+         
+        int h, b;
+        double area = 0;
+        System.out.print("Enter height: ");
+        h = sc.nextInt();
+        System.out.print("Enter base: ");
+        b = sc.nextInt();
+        area = (b*h)/2;
+        
+        
+        System.out.println("Area of triangle is: " + area);
+    }
+}    
 
