57634673929b28f5c874bc3c9bc187060532f438
Archit <115338571+a4archit@users.noreply.github.com>
2024-09-23 06:57:34 +0530

    Update Day19_area_of_rectangle.java

--- Program files/Day19_area_of_rectangle.java
+++ Program files/Day19_area_of_rectangle.java
@@ -1,1 +1,18 @@
-// area of rectangle
+import java.util.Scanner;
+
+class Day19_area_of_rectangle {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+         
+        int l, b, area = 0;
+        System.out.print("Enter length: ");
+        l = sc.nextInt();
+        System.out.print("Enter breath: ");
+        b = sc.nextInt();
+        area = l * b;
+        
+        
+        System.out.println("Area of rectangle is: " + area);
+    }
+}    
+
