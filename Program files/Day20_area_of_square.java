d5ce37b28746506bc66c530d57fb3ffba90663f2
Archit <115338571+a4archit@users.noreply.github.com>
2024-09-23 08:11:31 +0530

    Update Day20_area_of_square.java

--- Program files/Day20_area_of_square.java
+++ Program files/Day20_area_of_square.java
@@ -1,1 +1,18 @@
-// area of square
+
+import java.util.Scanner;
+
+class Day20_area_of_square {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+         
+        int l;
+      	double area = 0;
+        System.out.print("Enter length: ");
+        l = sc.nextInt();
+        area = Math.pow(l,2);
+        
+        
+        System.out.println("Area of rectangle is: " + area);
+    }
+}    
+
