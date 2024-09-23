75e352986c38601482db772afb9b950a9aa2b4cc
Archit <115338571+a4archit@users.noreply.github.com>
2024-09-23 17:44:07 +0530

    Create Day31_armstrong.java

---
+++ Program files/Day31_armstrong.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+class Day31_armstrong {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int n;
+        String armstrong_msg;
+        
+        // taking user input
+        System.out.print("Enter a number: ");
+        n = sc.nextInt();
+        
+        armstrong_msg = isPalindrome(n);
+        
+        System.out.println(n + " is " + armstrong_msg);
+        
+    }
+    
+    public static String isPalindrome(int n){
+        
+        // getting number of digits
+        int digits = 0;
+        int temp = n;
+        while (temp > 0){
+            digits++;
+            temp /= 10;
+        }
+        
+        int sum = 0;
+        temp = n;
+        for (int i=0; i<digits; i++){
+            
+            int ld = temp%10;
+            sum += Math.pow(ld, digits);
+            temp /= 10;
+        }
+        
+        return (sum == n)?"armstrong":"not an armstrong";
+    }
+}    
