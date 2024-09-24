c4ceaf8351e66c3916efe4aebeed10032ef398dd
Archit <115338571+a4archit@users.noreply.github.com>
2024-09-24 17:05:36 +0530

    Create Day35_print_array_using_arrays_class.java

---
+++ Program files/Day35_print_array_using_arrays_class.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class Day35_print_array_using_arrays_class {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int arr[] = {1,2,3,4,5,6,7,8,9};
+        
+        System.out.print(Arrays.toString(arr));
+        
+    }
+}
