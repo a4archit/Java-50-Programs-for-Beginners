8fd144c1e681fea7e8103615915fa2c0b53a55b1
Archit <115338571+a4archit@users.noreply.github.com>
2024-09-25 11:40:08 +0530

    Create Day37_length_of_array.java

---
+++ Program files/Day37_length_of_array.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class Day37_length_of_array {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int arr[] = {1,2,3,4,5,6,7,8,9};
+        
+        int arr_length = arr.length;
+        
+        System.out.println(Arrays.toString(arr) + "\n\nLength of the upper array = " + arr_length);
+        
+    }
+}    
