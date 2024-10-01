f77a9e8322bc1137f33ef1e3831b1b8ca962d7f4
Archit <115338571+a4archit@users.noreply.github.com>
2024-10-01 09:00:44 +0530

    Update Day39_array_addition.java

--- Program files/Day39_array_addition.java
+++ Program files/Day39_array_addition.java
@@ -1,8 +1,34 @@
-// here we perform arrays addition using Arrays class
+import java.util.Scanner;
+import java.util.Arrays;
 
 class Day39_array_addition {
-	public static void main(String args[]){
-
-
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int sum[];
+        int a[] = {1,2,3,4,5};
+        int b[] = {1,0,1,0,1};
+        
+        sum = add(a,b);
+        
+        System.out.println(" " + 
+             "\na = " + Arrays.toString(a) + 
+             "\nb = " + Arrays.toString(b) + 
+             "\na+b = " + Arrays.toString(sum));
+        
+    }
+    
+    public static int[] add(int a[], int b[]){
+        if (a.length != b.length){
+            System.out.println("Arrays a and b must of same length");
+        }
+        
+        int sum[] = Arrays.copyOf(a,a.length);
+        
+        for (int i=0; i<a.length; i++){
+            sum[i] = a[i] + b[i];
+        }
+        
+        return sum;
     }
 }
