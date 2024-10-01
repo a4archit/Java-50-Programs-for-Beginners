e421ae1a25ddb765997f1ce5196acc45e210cd9c
Archit <115338571+a4archit@users.noreply.github.com>
2024-10-01 12:05:20 +0530

    Update Day41_array_in_ascending_order.java

--- Program files/Day41_array_in_ascending_order.java
+++ Program files/Day41_array_in_ascending_order.java
@@ -1,1 +1,25 @@
-// arrays in ascending order
+
+import java.util.Arrays;
+
+class Day41_array_in_ascending_order {
+    public static void main(String[] args) {
+        
+        int arr[] = {1,6,3,9,2};
+        int asc[] = ascending(arr);
+        System.out.println(Arrays.toString(asc));
+    }
+
+    public static int[] ascending(int arr[]){
+        for (int i=0; i<arr.length; i++){
+            for (int j=i; j<arr.length; j++){
+                if (arr[i] > arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        
+        return arr;
+    }
+}
