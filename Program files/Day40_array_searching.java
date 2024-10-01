76617b4e39dee3da47f839e94cb23e8a3435326f
Archit <115338571+a4archit@users.noreply.github.com>
2024-10-01 12:33:26 +0530

    Update Day40_array_searching.java

--- Program files/Day40_array_searching.java
+++ Program files/Day40_array_searching.java
@@ -1,2 +1,26 @@
-// array searching
-// main class
+import java.util.Scanner;
+import java.util.Arrays;
+
+class Day40_array_searching {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int arr[] = {1,2,3,4,5};
+        int element;
+        
+        System.out.println("Array = " + Arrays.toString(arr) + "\n");
+        System.out.print("Enter element for search: ");
+        element = sc.nextInt(); 
+        
+        for(int i=0; i<arr.length; i++){
+            if (arr[i] == element){
+                System.out.println(element + " found in array.");
+                return;
+            }
+        }
+        
+        System.out.println(element + " not found in array.");
+        
+        
+    }
+}
