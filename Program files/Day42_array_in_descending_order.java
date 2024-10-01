a5a86ca929edf94f010045cc7bd5620a9a183eca
Archit <115338571+a4archit@users.noreply.github.com>
2024-10-01 12:06:48 +0530

    Update Day42_array_in_descending_order.java

--- Program files/Day42_array_in_descending_order.java
+++ Program files/Day42_array_in_descending_order.java
@@ -1,1 +1,25 @@
 
+import java.util.Arrays;
+
+class Day42_array_in_descending_order {
+    public static void main(String[] args) {
+      
+        int arr[] = {1,6,3,9,2};
+        int dsc[] = ascending(arr);
+        System.out.println(Arrays.toString(dsc));
+    }
+    
+    public static int[] descending(int arr[]){
+        for (int i=0; i<arr.length; i++){
+            for (int j=i; j<arr.length; j++){
+                if (arr[i] < arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        
+        return arr;
+    }
+}
